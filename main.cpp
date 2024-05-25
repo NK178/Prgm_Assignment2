0c0ef35ca5f8cb9c00996ee45c2081e3fbbb4b6c
Weird243 <243153G@mymail.nyp.edu.sg>
2024-05-25 17:51:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,24 @@ using namespace std;
 
 int main()
 {
+    int option;
+    // Ask user what they want to do 
     std::cout << "Welcome To Speed Typing";
-    std::cout << "Please choose what you want do";
+    std::cout << "Please choose what you want to do";
+    std::cout << "[1] Type ";
+    std::cout << "[2] Check Score ";
+    std::cin >> option;
+    // Options if they pick if they want to Type , Check Leaderboard or Quit
+    if (option == '1'){
+    }else if  (option == '2'){
+        
+    }else{
+        break;
+        return 0;
+    }
+    
+        
+
     
     
     
