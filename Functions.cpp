c34ea38948728c6e63d4eaad29f7c3b470637eac
NK178 <ayhngern@gmail.com>
2024-06-01 15:39:35 +0800

    Added Frame of UI function

--- Functions.cpp
+++ Functions.cpp
@@ -1,5 +1,14 @@
 //Code for all the functions 
-#include <Windows.h> // For cursor control 
+// For console control 
+#include <Windows.h>  
+// For ASCII code for UI 
+#define VERTUBE 186
+#define HORITUBE 205
+#define TOPRIGHT 187
+#define BOTTOMRIGHT 188
+#define BOTTOMLEFT 200
+#define TOPLEFT 201
+
 
 //Code to handle cursor control 
 void gotoxy(short x, short y) {
@@ -11,6 +20,31 @@ void gotoxy(short x, short y) {
 	//Gives control over cursor 
 	SetConsoleCursorPosition(hStdout, position);
 }
+
+//Code for frame of UI
+void Frame() {
+	//Spaces 
+	string space = "                                                                                                  ";
+	//Starting from left
+	cout << char(TOPLEFT);
+	for (int i = 0; i < 98; i++) {
+		cout << char(HORITUBE);
+	}
+	//Top right
+	cout << char(TOPRIGHT) << endl;
+	for (int i = 0; i < 27; i++) {
+		cout << char(VERTUBE);
+		cout << space << char(VERTUBE) << endl;
+	}
+	//Bottom left 
+	cout << char(BOTTOMLEFT);
+	for (int i = 0; i < 98; i++) {
+		cout << char(HORITUBE); 
+	}
+	//Bottom right 
+	cout << char(BOTTOMRIGHT) << endl;
+}
+
 
 //Code to handle typing 
 void Typeinput (char input[50], float total) {
