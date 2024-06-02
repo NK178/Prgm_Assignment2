31bdf1deb23857b0691ac84c192538ee4f8183ee
NK178 <ayhngern@gmail.com>
2024-06-02 22:28:46 +0800

    Final changes

--- Functions.cpp
+++ Functions.cpp
@@ -1,13 +1,5 @@
-//Code for all the functions 
-// For console control 
-#include <Windows.h>  
-// For ASCII code for UI 
-#define VERTUBE 186
-#define HORITUBE 205
-#define TOPRIGHT 187
-#define BOTTOMRIGHT 188
-#define BOTTOMLEFT 200
-#define TOPLEFT 201
+#include "Include.h"
+
 
 
 //Code to handle cursor control 
@@ -16,14 +8,15 @@ void gotoxy(short x, short y) {
 	//Use hStdout to gain control over the console
 	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 	//Delare x,y position of cursor
-	COORD position = {x,y};
+	COORD position = { x,y };
 	//Gives control over cursor 
 	SetConsoleCursorPosition(hStdout, position);
 }
 
+
 //Code for frame of UI
 void Frame() {
-
+	gotoxy(0, 0);
 	//Starting from left
 	cout << char(TOPLEFT);
 	for (int i = 0; i < 98; i++) {
@@ -47,11 +40,12 @@ void Frame() {
 	cout << char(BOTTOMRIGHT) << endl;
 }
 
+
 //Code for option 1 TYPE
 void Option1() {
 
 	//goto loction 
-	gotoxy(5, 7); 
+	gotoxy(5, 7);
 
 	//Starting from left
 	cout << char(TOPLEFTLI);
@@ -88,11 +82,12 @@ void Option1() {
 
 }
 
+
 //Code for option 2 LEADERBOARD
 void Option2() {
 
 	//goto loction 
-	gotoxy(55, 7); 
+	gotoxy(55, 7);
 
 	//Starting from left
 	cout << char(TOPLEFTLI);
@@ -129,11 +124,12 @@ void Option2() {
 
 }
 
+
 //Code for option 3 QUIT
 void Option3() {
 	//goto loction 
-	gotoxy(40, 17); 
-	
+	gotoxy(40, 17);
+
 	//Starting from left
 	cout << char(TOPLEFTLI);
 	for (int i = 0; i < 32; i++) {
@@ -167,6 +163,7 @@ void Option3() {
 	cout << "Press enter when pointer is here";
 }
 
+
 //Code for printing arrow symbol
 void Arrow(int x, int y) {
 
@@ -184,9 +181,10 @@ void Arrow(int x, int y) {
 
 }
 
+
 //Code for clearing arrows
 void Clear() {
-	
+
 	//Clear left most arrow
 	int x = 2, y = 8; /* (2, 8);*/
 	for (int i = 0; i < 5; i++) {
@@ -211,29 +209,248 @@ void Clear() {
 	}
 }
 
-//Code to handle typing 
-void Typeinput (char input[50], float total) {
 
-	//for getline to work, need ignore &  clear
-	cin.ignore();
-	cin.clear();
-	cin.getline(input, total);
-	cout << "\n: " << input << endl; 
+//Code to clear console of all text
+void ConsoleClear() {
+	gotoxy(0, 0);
+	for (int i = 0; i < 29; i++) {
+		//Clear horizontol 
+		for (int j = 0; j < 101; j++) {
+			cout << " ";
+		}
+		cout << endl;
+	}
+}
+
+//Code to make typing platform
+void Textbox() {
+	gotoxy(13, 14);
+	//printing out line
+	for (int i = 0; i < 75; i++) {
+		cout << char(HORILINE);
+	}
+}
+
+//Code to output sentence
+void Text(char SentenceFin[],float total) {
+
+	gotoxy(13, 13);
+	for (int i = 0; i < total; i++) {
+		cout << SentenceFin[i];
+	}
+	//if (total < 85) {
+	//	gotoxy(13, 13);
+	//	for (int i = 0; i < total; i++) {
+	//		cout << SentenceFin[i];
+	//	}
+	//}
+	//else if (total > 86) {
+	//	gotoxy(13, 12);
+	//	for (int i = 0; i < 65; i++) {
+	//			cout << SentenceFin[i];
+	//	}
+	//	//continue in next line
+	//	gotoxy(13, 13);
+	//	for (int i = 64; i < total; i++) {
+	//		cout << SentenceFin[i];
+	//	}
+	//}
+
+
 }
 
 //Code for accuracy checker 	
-void Errorcount (char input[50], char SentOutput [], float total, int& error_count) {
+void Errorcount(char input[], char SentOutput[], float total, int& error_count) {
 	//Comparing what user type and sentence 
-	for (int i = 0; i < total; i++){
+	for (int i = 0; i < total; i++) {
 		if (input[i] != SentOutput[i]) {
-		error_count++;
+			error_count++;
 		}
 	}
-	cout << "No. of mistakes" << error_count << endl;
+	gotoxy(13, 18);
+	cout << "No. of mistakes: " << error_count << endl;
 }
 
+
 //Code for accuracy calculator 
-void Accuracy (float& acc, float total, int error_count) {
+void Accuracy(float& acc, float total, int error_count) {
 	acc = ((total - error_count) / total) * 100;
-	cout << "Accuracy: " << acc; 
-} 
+	gotoxy(13, 19);
+	cout << "Accuracy: " << acc;
+}
+
+//asking user if they want to save their score 
+void SaveScore() {
+	gotoxy(12, 13);
+	for (int i = 0; i < 13; i++) {
+		for (int j = 0; j < 85; j++) {
+			cout << " ";
+		}
+		gotoxy(12, 13 + i);
+	}
+
+	gotoxy(25, 15);
+	cout << "Do you want to save your score?";
+	gotoxy(25, 16);
+	cout << "Press ENTER to save or BACKSPACE to return to menu";
+	gotoxy(25, 18);
+	//condition
+	int c = 0;
+
+	switch (c = _getch()) {
+
+	case BACKSPACE:
+		//reload main menu screen
+		ConsoleClear();
+		break;
+
+	case ENTER:
+		//Input name 
+		cout << "Input Name (No spaces):";
+		string place;
+		cin >> place;
+		ConsoleClear();
+		break;
+
+
+	}
+
+
+}
+
+//Code for retrying the typing game again
+void Retry(int &pos, bool &save) {
+	gotoxy(15, 23);
+	cout << "Do you want to retry?";
+	gotoxy(13, 24);
+	cout << "Press ENTER to try again or BACKSPACE to return to menu";
+	gotoxy(13, 25);
+	//condition
+	int c = 0;
+
+		switch (c = _getch()) {
+
+		case BACKSPACE:
+			//reload main menu screen
+			pos = 0;
+			SaveScore();
+			break;
+
+		case ENTER:
+			////erase previous typing
+			//bool save = false;
+			//gotoxy(13, 16);
+			//for (int i = 0; i < 8; i++) {
+
+			//	for (int j = 0; j < 80; j++) {
+			//		cout << " ";
+			//	}
+			//	gotoxy(13, 15 + i);
+			//}
+			////erase retry statement
+			//gotoxy(15, 23);
+			//cout << "                      ";
+			//gotoxy(13, 24);
+			//cout << "                                                       ";
+			ConsoleClear();
+			break;
+
+		}
+
+}
+
+
+
+
+
+//Code for resetting values 
+void ResetValues(float &total, int &error_count, float &acc, int &WPM, bool &Savescore) {
+	//reset all values
+	total = 0;
+	error_count = 0;
+	acc = 0;
+	WPM = 0;
+	Savescore = false;
+
+	//reset timer positon
+	gotoxy(35, 11);
+	cout << "     ";
+}
+
+
+//void FullReset() {
+//	Frame();
+//	Option1();
+//	Option2();
+//	Option3();
+//	ConsoleClear();
+//}
+
+//Counts down from 3 
+void Countdown() {
+
+	//3 second pause
+	int pause = 3;
+	//second
+	seconds _1sec = seconds(1);
+	//Go to location
+	
+
+	while (pause >= 1) {
+		gotoxy(35, 11);
+		cout << pause;
+
+		//Pause for 1 second
+		this_thread::sleep_for(_1sec);
+		pause--;
+		if (pause < 1) {
+			gotoxy(35, 11);
+			cout << " GO !";
+		}
+	}
+
+
+}
+
+////To choose a randome sentence 
+//void Random() {
+//
+//	// Providing a seed value for randomizer so value can be different
+//	srand((unsigned)time(NULL));
+//
+//	// Offset = 1
+//	// Range = 5
+//	int random = 1 + (rand() % 5);
+//
+//	switch (random) {
+//
+//	case 1:
+//
+//	}
+//}
+
+
+
+
+////Tracks how much time user spent typing
+////void Timer(clock_t start, clock_t stop, double &fintime) {
+////
+////	float Totaltime;
+////	Totaltime = (stop - start) / (double)CLOCKS_PER_SEC;
+////
+////}
+//
+////Calculates words per minute
+//void WPM(float fintime, float total, int &WPM) {
+//
+//	//WPM =  characters/5 * 1/time 
+//	//5 used to represent average lenght of words
+//	WPM = (total / 5) * (1 / fintime);
+//	
+//
+//
+//}
+
+//void Leaderboard() {
+//
+//}
