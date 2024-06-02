b64702a8f549921e51ca76264eea502d7766f65f
NK178 <ayhngern@gmail.com>
2024-06-02 12:02:28 +0800

    Added a bunch code, basically made the whole menu

--- Functions.cpp
+++ Functions.cpp
@@ -23,28 +23,193 @@ void gotoxy(short x, short y) {
 
 //Code for frame of UI
 void Frame() {
-	//Spaces 
-	string space = "                                                                                                  ";
+
 	//Starting from left
 	cout << char(TOPLEFT);
 	for (int i = 0; i < 98; i++) {
 		cout << char(HORITUBE);
 	}
+
 	//Top right
 	cout << char(TOPRIGHT) << endl;
 	for (int i = 0; i < 27; i++) {
 		cout << char(VERTUBE);
-		cout << space << char(VERTUBE) << endl;
+		gotoxy(99, 1 + i);
+		cout << char(VERTUBE) << endl;
 	}
+
 	//Bottom left 
 	cout << char(BOTTOMLEFT);
 	for (int i = 0; i < 98; i++) {
-		cout << char(HORITUBE); 
+		cout << char(HORITUBE);
 	}
 	//Bottom right 
 	cout << char(BOTTOMRIGHT) << endl;
 }
 
+//Code for option 1 TYPE
+void Option1() {
+
+	//goto loction 
+	gotoxy(5, 7); 
+
+	//Starting from left
+	cout << char(TOPLEFTLI);
+	for (int i = 0; i < 40; i++) {
+		cout << char(HORILINE);
+	}
+
+	//Top right
+	cout << char(TOPRIGHTLI) << endl;
+	for (int i = 0; i < 5; i++) {
+		gotoxy(5, 8 + i);
+		cout << char(VERLINE);
+		gotoxy(46, 8 + i);
+		cout << char(VERLINE) << endl;
+	}
+
+	//Bottom left 
+	gotoxy(5, 13);
+	cout << char(BOTTOMLEFTLI);
+	for (int i = 0; i < 40; i++) {
+		cout << char(HORILINE);
+	}
+	//Bottom right 
+	cout << char(BOTTOMRIGHTLI) << endl;
+
+	//cout "type"
+	gotoxy(6, 8);
+	cout << "                " << "TYPING TEST";
+	gotoxy(6, 9);
+	cout << "       " << "Improve your typing skills";
+	gotoxy(6, 11);
+	cout << "     " << "Press enter when pointer is here";
+
+
+}
+
+//Code for option 2 LEADERBOARD
+void Option2() {
+
+	//goto loction 
+	gotoxy(55, 7); 
+
+	//Starting from left
+	cout << char(TOPLEFTLI);
+	for (int i = 0; i < 40; i++) {
+		cout << char(HORILINE);
+	}
+
+	//Top right
+	cout << char(TOPRIGHTLI) << endl;
+	for (int i = 0; i < 5; i++) {
+		gotoxy(55, 8 + i);
+		cout << char(VERLINE);
+		gotoxy(96, 8 + i);
+		cout << char(VERLINE) << endl;
+	}
+
+	//Bottom left 
+	gotoxy(55, 13);
+	cout << char(BOTTOMLEFTLI);
+	for (int i = 0; i < 40; i++) {
+		cout << char(HORILINE);
+	}
+	//Bottom right 
+	cout << char(BOTTOMRIGHTLI) << endl;
+
+	//cout "type"
+	gotoxy(56, 8);
+	cout << "                " << "LEADERBOARD";
+	gotoxy(56, 9);
+	cout << "     " << "See how you fare against others!";
+	gotoxy(56, 11);
+	cout << "     " << "Press enter when pointer is here";
+
+
+}
+
+//Code for option 3 QUIT
+void Option3() {
+	//goto loction 
+	gotoxy(40, 17); 
+	
+	//Starting from left
+	cout << char(TOPLEFTLI);
+	for (int i = 0; i < 32; i++) {
+		cout << char(HORILINE);
+	}
+
+	//Top right
+	cout << char(TOPRIGHTLI) << endl;
+	for (int i = 0; i < 5; i++) {
+		gotoxy(40, 18 + i);
+		cout << char(VERLINE);
+		gotoxy(73, 18 + i);
+		cout << char(VERLINE) << endl;
+	}
+
+	//Bottom left 
+	gotoxy(40, 23);
+	cout << char(BOTTOMLEFTLI);
+	for (int i = 0; i < 32; i++) {
+		cout << char(HORILINE);
+	}
+	//Bottom right 
+	cout << char(BOTTOMRIGHTLI) << endl;
+
+	//cout "type"
+	gotoxy(41, 19);
+	cout << "             " << "QUIT";
+	gotoxy(41, 20);
+	cout << "       " << "Hope to see you soon!";
+	gotoxy(41, 21);
+	cout << "Press enter when pointer is here";
+}
+
+//Code for printing arrow symbol
+void Arrow(int x, int y) {
+
+	gotoxy(x, y);
+
+	cout << "*";
+	gotoxy(x, y + 1);
+	cout << "**";
+	gotoxy(x, y + 2);
+	cout << "***";
+	gotoxy(x, y + 3);
+	cout << "**";
+	gotoxy(x, y + 4);
+	cout << "*";
+
+}
+
+//Code for clearing arrows
+void Clear() {
+	
+	//Clear left most arrow
+	int x = 2, y = 8; /* (2, 8);*/
+	for (int i = 0; i < 5; i++) {
+		gotoxy(x, y + i);
+		cout << "   ";
+	}
+
+	x = 52;
+	y = 8; /*(50, 8);*/
+	//Clear right most arrow
+	for (int i = 0; i < 5; i++) {
+		gotoxy(x, y + i);
+		cout << "   ";
+	}
+
+	x = 37;
+	y = 18; /*(37, 18);*/
+	//Clear bottom arrow
+	for (int i = 0; i < 5; i++) {
+		gotoxy(x, y + i);
+		cout << "   ";
+	}
+}
 
 //Code to handle typing 
 void Typeinput (char input[50], float total) {
